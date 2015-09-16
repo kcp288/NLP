59a63f2c866de1839b730389da4745a610792edf
kcp288 <kira.prentice@nyu.edu>
2015-09-16 14:13:49 -0400

    only searches lines

--- HW2.py
+++ HW2.py
@@ -1,31 +1,59 @@
 # KiraPrentice-HW2.py
 import nltk, re
 
-input_file = open('./NLP_HW1.txt', 'r')
+pattern = re.compile(r'[0-9]{3}(-| )[0-9]{3}(-| )[0-9]{4}')
+
+input_file = open('./example.txt', 'r').readlines()
 output = open('./output_file.txt', 'w')
 
+#text = input_file.read()
+#print text
+
+x = ''
+for line in input_file:
+	s = line.split()
+	# search
+	match = pattern.search(line)
+	if match: 
+		print(match.group(0))
+	
+
+
+'''
+
+for line in words:
+	if "AA" in line: 
+		# Split items into list items
+		s = line.split()
+
+		# See if "AA" is within word
+		if s.count("AA") == 0:
+			continue	
+		else:
+			ndx = s.index("AA")
+			count_aa += 1
+for w in input_file:
+	if re.search()
+for line in input_file:
+	for w in line:
+		if re.search(r'[0-9]', line)
+		print 
+	for match in re.finditer(pattern, line):
+		print(match.groups())
+
+
+
 raw = input_file.read()
-text1 = raw.split()
-tokens = nltk.word_tokenize(raw)
-text = nltk.Text(text1)
 
-print(text.concordance("adjective"))
 
+
+#tokens = nltk.word_tokenize(raw2)
+#text = nltk.Text(raw)
+
+#print(text);
+
+
+print("Done")
 '''
-output.write(
-	[w for w in text if re.search('s/(19|20)[0-9][0-9]/[&]/g', w)]) 
-
-output.write (
-	text.findall(r"<\w*> <and> <other> <\w*s>"))
-
-
-
-// Identify dollar amounts
-
-'s/(19|20)[0-9][0-9]/[&]/g' all-OANC.txt > output_file
-
-// Identify telephone numbers
-'''
-
-text.close()
+#input_file.close()
 output.close()
